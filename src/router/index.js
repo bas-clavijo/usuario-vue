b938111e7253a9a850e5100aa8321be0f44e1506
bas-clavijo <bas.clavijo@duocuc.cl>
2024-05-02 14:35:11 -0400

    Se importan las vistas

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import UsuarioNew from '../views/UsuarioNew.vue'
+import UsuarioEdit from '../views/UsuarioEdit.vue'
+import UsuarioView from '../views/UsuarioView.vue'
 
 const routes = [
   {
