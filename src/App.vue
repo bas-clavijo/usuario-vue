113befea1a71ac83fa97c7331b35099c9f4b6259
bas-clavijo <bas.clavijo@duocuc.cl>
2024-05-02 18:10:15 -0400

    Creacion de menu y header

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,30 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <!-- Encabezado -->
+  <nav class="navbar bg-body-secondary">
+  <div class="container-fluid">
+    <a class="navbar-brand">TEST NEERING</a>
+    <form class="d-flex" role="search">
+      <a class="navbar-brand">Nombre usuario</a>
+    </form>
+  </div>
+</nav>
+
+ <!-- Menú lateral -->
+ <div class="d-flex" id="wrapper">
+        <!-- Sidebar -->
+        <div class="bg-light border-right" id="sidebar-wrapper">
+            <div class="list-group list-group-flush">
+                <router-link to="/home" class="list-group-item list-group-item-action">Menú</router-link>
+                <router-link to="/tabla" class="list-group-item list-group-item-action">Tabla</router-link>
+                <router-link to="/createUsuario" class="list-group-item list-group-item-action">Formulario</router-link>
+            </div>
+        </div>
+    </div>
+
+
+<div class="container-fluid">
   <router-view/>
+</div>
 </template>
+
 
