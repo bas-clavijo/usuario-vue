7f8982b6649b5086d93612b9e7e8f27a95070b0e
bas-clavijo <bas.clavijo@duocuc.cl>
2024-05-06 11:50:03 -0400

    Modificacion de funciones

--- src/funciones.js
+++ src/funciones.js
@@ -26,13 +26,35 @@ export function confirmar(Url,id,titulo,mensaje){
         text:mensaje,
         icon:'question',
         showCancelButton:true,
-        confirmButtonText:''
-    });
+        confirmButtonText:'<i class="fa-solid fa-check"></i> Si,eliminar',
+        cancelButtonText:'<i class="fa-solid fa-ban"></i> Cancelar'}).then((res)=>{
+            if(res.isConfirmed){
+                enviarSolicitud('DELETE',{id:id},url,'Eliminado con exito');
+            }else{
+                mostrarAlerta('Operacion cancelada', 'info');
+            }
+        });
 
     Swal.fire({
         title:titulo,
         icon:icono,
         customClass:{confirmButton:'btn btn-primary', popup:'animated zoonIn'},
         buttonsStyling:false
+    });
+}
+
+export function enviarSolicitud(metodo,parametros,url,mensaje){
+    axios({method:metodo,url:url,data:parametros}).then(function(res){
+        var estado = res.status;  
+        if(estado == 200){
+            mostrarAlerta(mensaje, 'success');
+            window.setTimeout(function(){
+                window.location.href='/tabla'
+            },1000);
+        }else{
+            mostrarAlerta('No se ha podido recuperar la respuesta', 'error');
+        }
+    }).catch(function(error){
+        mostrarAlerta('Error del servidor', 'error');
     });
 }
