58dcfe3541cc11459f0992d3f552be1eeade1939
bas-clavijo <bas.clavijo@duocuc.cl>
2024-05-06 00:50:48 -0400

    Modificacion de funcion

--- src/funciones.js
+++ src/funciones.js
@@ -13,3 +13,26 @@ export function mostrarAlerta(titulo,icono,foco=''){
         buttonsStyling:false
     });
 }
+
+export function confirmar(Url,id,titulo,mensaje){
+
+    var url = Url+id;
+    const swalWithBoostrapButton = Swal.mixin({
+        customClass:{confirmButton:'btn btn-succes me-3', cancelButton:'btn btn-danger'},
+    });
+
+    swalWithBoostrapButton.fire({
+        title:titulo,
+        text:mensaje,
+        icon:'question',
+        showCancelButton:true,
+        confirmButtonText:''
+    });
+
+    Swal.fire({
+        title:titulo,
+        icon:icono,
+        customClass:{confirmButton:'btn btn-primary', popup:'animated zoonIn'},
+        buttonsStyling:false
+    });
+}
