294549ee99d1017c9f11906455424937667e4df3
bas-clavijo <bas.clavijo@duocuc.cl>
2024-05-08 01:14:57 -0400

    Modificaciones al diseño

--- src/App.vue
+++ src/App.vue
@@ -6,22 +6,10 @@
         <img src="@/assets/logo.png" width="34">
         <a class="navbar-brand ms-2">TEST NEERING</a>
       </div>
-
-      <div class="d-flex align-items-center">
-        <!-- Icono de persona -->
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-person-circle"
-          viewBox="0 0 16 16">
-          <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
-          <path fill-rule="evenodd"
-            d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1" />
-        </svg>
-        <!-- Nombre del usuario -->
-        <a class="navbar-brand">Nombre usuario</a>
-      </div>
     </div>
   </nav>
   
-  <!-- Menú lateral 2 -->
+  <!-- Menú lateral -->
   <div class="bg-dark col-12 col-md-2 min-vh-100 position-fixed">
     <div class="bg-dark p-2">
       <a class="d-flex text-decoration-none mt-1 align-items-center text-white">
